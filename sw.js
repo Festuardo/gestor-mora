641a21f7ace666893fc9154e293b65a42a9e46b6
Franklin Garcia <frank.estuard.95@gmail.com>
2026-09-08 12:50:53 -0600

    Agregar gestores, modo instalable y funcionamiento sin conexión

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'gestor-mora-v2';
+const LOCAL = ['./', './index.html', './creditos.html', './tarjetas.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './vendor/xlsx.bundle.js', './vendor/jspdf.umd.min.js'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(LOCAL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      if (response && response.ok) {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
