e6823f74aa383752c057c93fe7082977f15ff2f0
Franklin Garcia <frank.estuard.95@gmail.com>
2026-09-20 10:05:06 -0600

    Actualizar caché v14 para acciones con iconos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gestor-mora-v13';
+const CACHE = 'gestor-mora-v14';
 const LOCAL = ['./', './index.html', './creditos.html', './tarjetas.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './vendor/xlsx.bundle.js', './vendor/jspdf.umd.min.js'];
 
 self.addEventListener('install', event => {
